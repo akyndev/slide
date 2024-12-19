fc82456aa0ddaa8deaf800c18d70b409deaac756
Joseph Akinlade <akinaldeirede@gmail.com>
2024-12-19 05:49:04 +0100

    removed env

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 import { Separator } from "@/components/ui/separator"
-import { ChevronsDownIcon } from "lucide-react"
+import { LogoSmall } from "@/svgs/logo-small"
 import Link from "next/link"
 
 export default function Footer() {
@@ -9,9 +9,9 @@ export default function Footer() {
         <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
           <div className="col-span-full xl:col-span-2">
             <Link href="#" className="flex font-bold items-center">
-              <ChevronsDownIcon className="w-9 h-9 mr-2 bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg border border-secondary" />
-
-              <h3 className="text-2xl">Shadcn</h3>
+              <div className="flex gap-x-2 items-center p-0 justify-center">
+                <LogoSmall />
+              </div>
             </Link>
           </div>
 

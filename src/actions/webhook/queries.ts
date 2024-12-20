2e24c029d1cee4c3d2d541023e49a19cd80e0965
Joseph Akinlade <akinaldeirede@gmail.com>
2024-12-20 03:09:25 +0100

    working on normal messaging

--- src/actions/webhook/queries.ts
+++ src/actions/webhook/queries.ts
@@ -9,6 +9,7 @@ const increment = (column: AnyColumn, value = 1) => {
 }
 
 export const getChatHistory = async (recieverId: string, senderId: string) => {
+  console.log(recieverId, senderId)
   const history = await db.query.dms.findMany({
     where: and(eq(dms.senderId, senderId), eq(dms.reciever, recieverId)),
     orderBy: [asc(dms.createdAt)]
